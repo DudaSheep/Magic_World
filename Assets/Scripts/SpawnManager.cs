1e13fb238cf792c407420b104cf38e2f8d673451
Maria Eduarda <meduarda.carneiro@hotmail.com>
2026-04-21 21:53:07 -0300

    feat: new Enemy Ghost, pathfinder (perseguição), stage 2

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,16 +6,22 @@ public class SpawnManager : MonoBehaviour
 {
 
     public GameObject enemyPrefab;
+    public GameObject enemyGhostPrefab;
+
     public float timeSpawn = 2f; // Tempo de aparecimento dos enemys
     public float minHeight = -3f;
     public float maxHeight = 2f;
 
+    // ConfiguraÃ§oes de Fase
+    public float timeToStartGhosts = 20f; // tempo para comecar a aparecer o ghost fireballs
+    private float gameTimer = 0f; //tempo de game
+
     // Start is called before the first frame update
     void Start()
     {
-        // Começa a chamar a função de criar inimigos InvokeRepeating("NomeDaFuncao", tempoParaComeçar, intervaloEntreRepetições)
+        // Comeï¿½a a chamar a funï¿½ï¿½o de criar inimigos InvokeRepeating("NomeDaFuncao", tempoParaComeï¿½ar, intervaloEntreRepetiï¿½ï¿½es)
         InvokeRepeating("Spawn", 1f, timeSpawn);
-        // Chama a função que aumenta a dificuldade a cada 7 segundos
+        // Chama a funï¿½ï¿½o que aumenta a dificuldade a cada 7 segundos
         InvokeRepeating("IncreaseDifficulty", 7f, 7f);
     }
 
@@ -23,9 +29,23 @@ public class SpawnManager : MonoBehaviour
     {
         // Altura random para o inimigo aparecer
         float posYRandom = Random.Range(minHeight, maxHeight);
-        Vector3 posSpawn = new Vector3(12f, posYRandom, 0); 
+        Vector3 posSpawn = new Vector3(12f, posYRandom, 0);
 
-        Instantiate(enemyPrefab, posSpawn, Quaternion.identity);
+        int ghostCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+        // Logica das enemy firaballs direfentes
+        if (gameTimer > timeToStartGhosts)
+        {
+            // Spawna a fantasma roxa - ghost fireballs
+            Instantiate(enemyGhostPrefab, posSpawn, Quaternion.identity);
+        }
+        else
+        {
+            // Spawna a Enemy_Fireball normal
+            Instantiate(enemyPrefab, posSpawn, Quaternion.identity);
+        }
+
+
     }
 
     void IncreaseDifficulty()
@@ -42,6 +62,6 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        gameTimer += Time.deltaTime; //contar o tempo real de game
     }
 }
