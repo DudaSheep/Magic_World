e8bdbcd85453539f5e97525163e3dca37c86c9ae
Maria Eduarda <meduarda.carneiro@hotmail.com>
2026-04-23 11:32:26 -0300

    feat: mecanismo de  gasto e recompensa special attack button

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,6 +21,11 @@ public class PlayerController : MonoBehaviour
     private Vector3 targetedMousePos;
 
 
+    [Header("Poder Especial")]
+    public int specialPowerCost = 150; // Custo do especial
+    public int numberOfProjectiles = 24; // numero de fireballs no especial
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -44,7 +49,7 @@ public class PlayerController : MonoBehaviour
             Flip();
         }
 
-        // Checar se esta querendo PULAR
+        // PULAR
         if (Input.GetButtonDown("Jump"))
         {
             animator.SetBool("Jump", true);
@@ -54,6 +59,7 @@ public class PlayerController : MonoBehaviour
 
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
+        // ATIRAR fireball - ataque normal
         if (Input.GetButtonDown("Fire1"))
         {
             // Salva a posicao exata do clique
@@ -61,6 +67,12 @@ public class PlayerController : MonoBehaviour
             targetedMousePos.z = 0;
 
             animator.SetTrigger("Attack");
+        }
+
+        // ATIRAR - ataque especial (Botão Direito ou E)
+        if (Input.GetButtonDown("Fire2") || Input.GetKeyDown(KeyCode.E))
+        {
+            TryActivateSpecial();
         }
 
         // Crouch (left shift ou c)
@@ -72,6 +84,51 @@ public class PlayerController : MonoBehaviour
         else
         {
             animator.SetBool("Crouch", false);
+        }
+    }
+
+    void TryActivateSpecial()
+    {
+        GameManager gm = FindObjectOfType<GameManager>();
+        // Verifica se tem score suficiente para usar o especial
+        if (gm != null && gm.SpendScore(specialPowerCost))
+        {
+            animator.SetTrigger("Attack");
+            Debug.Log("Especial Ativado!");
+            SpecialAttack();
+        }
+        else
+        {
+            Debug.Log("Score insuficiente para o especial!");
+        }
+    }
+
+    // ATAQUE ESPECIAL 360 graus de fireballs
+    void SpecialAttack()
+    {
+        float angleStep = 360f / numberOfProjectiles;
+        float angle = 0f;
+
+        for (int i = 0; i < numberOfProjectiles; i++)
+        {
+            // Calcula a direção baseada no ângulo para formar um círculo
+            float bulletDirX = Mathf.Cos(angle * Mathf.Deg2Rad);
+            float bulletDirY = Mathf.Sin(angle * Mathf.Deg2Rad);
+
+            Vector2 bulletDir = new Vector2(bulletDirX, bulletDirY).normalized;
+
+            // Instancia a fireball
+            GameObject bullet = Instantiate(fireballPrefab, firePoint.position, Quaternion.Euler(0, 0, angle));
+
+            // Aplica velocidade na direção do raio do circulo
+            Rigidbody2D rbBullet = bullet.GetComponent<Rigidbody2D>();
+            if (rbBullet != null)
+            {
+                rbBullet.velocity = bulletDir * fireballSpeed;
+            }
+
+            Destroy(bullet, 2f);
+            angle += angleStep;
         }
     }
 
